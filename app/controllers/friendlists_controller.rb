ce3af136a402cf43e07a710eda0eb7dce2617714
metrox <dawiddyderski@gmail.com>
2011-07-12 14:36:34 +0200

    alfa

--- app/controllers/friendlists_controller.rb
+++ app/controllers/friendlists_controller.rb
@@ -39,11 +39,11 @@ class FriendlistsController < ApplicationController
   # POST /friendlists
   # POST /friendlists.xml
   def create
-    @Cfriendlist = Friendlist.new(params[:friendlist])
-    @Cfriendlist[:id_user] = current_user.id
-    @Cfriendlist[:id_friend] = params[:id_friend]
+    @cfriendlist = Friendlist.new(params[:friendlist])
+    @cfriendlist[:id_user] = current_user.id
+    @cfriendlist[:id_friend] = params[:id_friend]
     respond_to do |format|
-      if @Cfriendlist.save
+      if @cfriendlist.save
         #format.html { redirect_to(@friendlist, :notice => 'Friendlist was successfully created.') }
         format.html { redirect_to(user_path(params[:id_friend])) }
         format.xml  { render :xml => @friendlist, :status => :created, :location => @friendlist }
